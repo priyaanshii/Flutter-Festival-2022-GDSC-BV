3ba024dcc6c12bc64a1c10cbc3212448566fd6a6
AnanyaSwarnkar <97191169+AnanyaSwarnkar@users.noreply.github.com>
2022-02-26 20:24:32 +0530

    Create question 4.dart

---
+++ TASK 2/question 4.dart
@@ -0,0 +1,34 @@
+void main() {
+  var details = {
+    1: 'a',
+    2: 'b',
+    3: 'c',
+    4: 'd',
+    5: 'e',
+    6: 'f',
+    7: 'g',
+    8: 'h',
+    9: 'i',
+    10: 'j',
+    11: 'k',
+    12: 'l',
+    13: 'm',
+    14: 'n',
+    15: 'o',
+    16: 'p',
+    17: 'q',
+    18: 'r',
+    19: 's',
+    20: 't',
+    21: 'u',
+    22: 'v',
+    23: 'w',
+    24: 'x',
+    25: 'z'
+  };
+  //print(details);
+  stdout.write('enter number from 1-26 :');
+  int? n = int.parse(stdin.readLineSync()!);
+  stdout.write('The corresponding letter to $n :');
+  print(details[n]);
+}
